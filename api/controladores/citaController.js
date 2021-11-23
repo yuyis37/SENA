85182c8c2cca40f81b2fcb15f6953fa62876e2b5
yuyis37 <88407006+yuyis37@users.noreply.github.com>
2021-11-22 20:31:52 -0500

    api

---
+++ api/controladores/citaController.js
@@ -0,0 +1,131 @@
+const { response } = require("express");
+const { request } = require("http");
+
+var citaModel = require(appRoot + "/api/modelos/citaModel.js").cita;
+
+var citaController = {};
+
+citaController.guardar = function(request,response){
+  var post = {
+    id:request.body.id,
+    nombre:request.body.nombre,
+    identificacion:request.body.identificacion,
+    telefono:request.body.telefono,
+    dia:request.body.dia,
+    hora:request.body.hora,
+    //_id:request.body.rol_id,
+  }
+  if(post.nombre == null || post.nombre == undefined || post.nombre =='' ){
+    response.json({state:false,mensaje:'campo obligatorio del nombre'})
+    return false;
+  }
+  if(post.identificacion == null || post.identificacion == undefined || post.identificacion =='' ){
+    response.json({state:false,mensaje:'campo obligatorio de la identificacion'})
+    return false;
+  }
+  if(post.telefono == null || post.telefono == undefined || post.telefono =='' ){
+    response.json({state:false,mensaje:'campo obligatorio del telefono'})
+    return false;
+  }
+  if(post.dia == null || post.dia == undefined || post.dia =='' ){
+    response.json({state:false,mensaje:'campo obligatorio del dia'})
+    return false;
+  }
+  if(post.hora == null || post.hora == undefined || post.hora =='' ){
+    response.json({state:false,mensaje:'campo obligatorio de la hora'})
+    return false;
+  }
+  console.log("----------------------> request");
+  console.log(post);
+
+  citaModel.guardar(post,function (respuesta) {
+    console.log("--------------->response");
+    console.log(respuesta);
+    response.json({state:true, data:respuesta});
+  });
+};
+
+citaController.listar = function (request, response) {
+    citaModel.listar(null,function (respuesta) {
+      console.log("--------------->response");
+      console.log(respuesta);
+      response.json(respuesta);
+    });
+    };
+    
+citaController.listarId = function(request,response){
+  var post={
+    id:request.body.id
+  }
+  if(post.id == null || post.id == undefined || post.id =='' ){
+    response.json({state:false,mensaje:'el campo id es obligatorio'})
+    return false;
+  }
+ citaModel.listarId(post,function(respuesta){
+   response.json(respuesta)
+ }) 
+}
+
+citaController.actualizar = function(request,response){
+  var paciente = {
+      nombre:request.body.nombre,
+      identificacion:request.body.identificacion,
+      telefono:request.body.telefono,
+      dia:request.body.dia,
+      hora:request.body.hora,
+      id:request.body.id
+  }
+  console.log('------------------->request')
+  console.log(paciente)
+
+  if(paciente.id == "" || paciente.id == undefined || paciente.id == null){
+      response.json({state:false,mensaje:'el campo id es obligatorio'})
+      return false;
+  }
+  if(paciente.nombre == "" || paciente.nombre == undefined || paciente.nombre == null){
+      response.json({state:false,mensaje:'el campo nombre es obligatorio'})
+      return false;
+  }
+  if(paciente.identificacion == "" || paciente.identificacion == undefined || paciente.identificacion == null){
+      response.json({state:false,mensaje:'el campo identificacion es obligatorio'})
+      return false;
+  }
+  if(paciente.telefono == "" || paciente.telefono == undefined || paciente.telefono == null){
+    response.json({state:false,mensaje:'el campo telefono es obligatorio'})
+    return false;
+}
+  if(paciente.dia == "" || paciente.dia == undefined || paciente.dia == null){
+      response.json({state:false,mensaje:'el campo dia es obligatorio'})
+      return false;
+  }
+  if(paciente.hora == "" || paciente.hora == undefined || paciente.hora == null){
+      response.json({state:false,mensaje:'el campo hora es obligatorio'})
+      return false;
+  }
+  citaModel.actualizar(paciente,function(respuesta){
+      console.log('------->response')
+      console.log(respuesta)
+      response.json(respuesta)
+  })
+}  
+citaController.eliminar = function (request, response) {
+  var post = {
+    id:request.body.id
+  }
+  if (
+   post.id == "" ||
+   post.id == undefined ||
+   post.id == null
+ ) {
+   response.json({ state: false, mensaje: "el campo id es obligatorio" });
+   return false;
+ }
+
+   citaModel.eliminar(post,function(resultado){
+     response.json(resultado)
+   })
+ };
+
+
+    module.exports.cita = citaController;
+ 
---
+++ api/modelos/pacienteModel.js
@@ -0,0 +1,94 @@
+var pacienteModel = {};
+
+const mongoose = require("mongoose");
+//const appConfig = require('config.js')
+
+const Schema = mongoose.Schema;
+
+var pacienteSchema = new Schema({
+  nombre: String,
+  identificacion:String,
+  email:String,
+  direccion: String,
+  telefono: String,
+  id:String,
+  
+ 
+});
+
+const Mymodel = mongoose.model('paciente', pacienteSchema);
+
+
+ pacienteModel.guardar = function(post,callback){
+
+const instancia = new Mymodel;
+instancia.nombre = post.nombre
+instancia.identificacion=post.identificacion
+instancia.email = post.email
+instancia.direccion=post.direccion
+instancia.telefono=post.telefono
+
+instancia.save((error,userCreate) =>{
+  if(error){
+    console.log(error);
+    return callback({state:false,info:error})
+  }
+  else{
+    console.log(userCreate)
+    return callback({state:true,info:userCreate})
+
+  }
+})
+ }
+   pacienteModel.listar = function (post, callback) {
+    Mymodel.find({},(error,documentos) => {
+      if (error) {
+        console.log(error);
+        return callback(error);
+      } else {
+        console.log(documentos);
+        return callback(documentos);
+      }
+    }) 
+  }
+pacienteModel.listarId = function(post, callback){
+  Mymodel.find({_id:post.id},{nombre:1,identificacion:1,email:1,_id:1,direccion:1,telefono:1},(err,registros)=>{
+    if(err){
+      console.log(err)
+      return callback(err)
+    } 
+    else{
+      return callback(registros)
+    }
+  })
+}
+pacienteModel.eliminar = function (post, callback) {
+  Mymodel.findByIdAndDelete(post.id ,(error,resultados) => {
+   if (error) {
+     console.log(error);
+     return callback(error);
+   } else {
+     return callback({state:true,mensaje:'Paciente Eliminado'});
+   }
+  })
+ }
+pacienteModel.actualizar = function (post, callback) {
+  Mymodel.findByIdAndUpdate(post.id, {
+    nombre:post.nombre,
+    identificacion:post.identificacion,
+    email:post.email,
+    direccion:post.direccion,
+    telefono:post.telefono
+  }, (error,pacmodificado) => {
+    if (error) {
+        console.log(error);
+        return callback(error);
+      } else {
+        console.log(pacmodificado);
+        return callback({state:true,mensaje:'Paciente Modificado'})
+      }
+  })
+};
+
+
+  module.exports.paciente =pacienteModel;
